5276142291ebb3b8ee687182807bbf7708e2585d
ALPHISTUBE <rsalphi@gmail.com>
2024-11-10 09:57:13 +0600

    update

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from 'react';
 import { Icon } from '@iconify/react';
 import Spline from '@splinetool/react-spline/next';
-const scene = () => {
+
+const Scene = () => {
     return (
-        <div>
+        <div className="relative w-screen h-screen">
             <Spline
                 scene="https://prod.spline.design/PBcOzeNKKp5lmiCW/scene.splinecode" 
             />
-            <div style={{ position: 'absolute', top: '10px', left: '10px' }}>
-                <Icon icon="mdi:keyboard-w" /> W
-                <Icon icon="mdi:keyboard-s" /> S
-                <Icon icon="mdi:keyboard-a" /> A
-                <Icon icon="mdi:keyboard-d" /> D
-                <Icon icon="mdi:space-invaders" /> Space
-                
-            </div>
-            <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-                Speed: 0
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-2 rounded text-white flex flex-col gap-1">
+                <div className=' bg-black bg-opacity-50'>
+                    <div className="flex items-center"><Icon icon="mdi:keyboard-w" /> W</div>
+                    <div className="flex items-center"><Icon icon="mdi:keyboard-s" /> S</div>
+                    <div className="flex items-center"><Icon icon="mdi:keyboard-a" /> A</div>
+                    <div className="flex items-center"><Icon icon="mdi:keyboard-d" /> D</div>
+                    <div className="flex items-center"><Icon icon="mdi:space-invaders" /> Space</div>
+                </div>
+                <div className="top-2 left-2 bg-black bg-opacity-50 p-2 rounded text-white">
+                    Speed: 0
+                </div>
             </div>
         </div>
     );
 };
 
-export default scene;
+export default Scene;
