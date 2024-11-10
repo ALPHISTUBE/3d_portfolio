04bf703ee6eeda2ed0bee7cea2f3fd450cf29c96
ALPHISTUBE <rsalphi@gmail.com>
2024-11-10 09:53:16 +0600

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import ThreeCanvas from '@/components/threecanvas'; // Importing the ThreeCanvas component
 import Scene from '@/components/scene';
 
 const Home = () => {
   return (
     <div>
-      <h1 className="text-center text-4xl text-white bg-black p-5">My 3D Project</h1>
-      
-      {/* Rendering the Three.js canvas */}
       <Scene /> 
     </div>
   );
--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
-import { useEffect } from 'react';
 import { Icon } from '@iconify/react';
-
+import Spline from '@splinetool/react-spline/next';
 const scene = () => {
     return (
         <div>
-            <iframe
-                src='https://my.spline.design/untitled-c770ba90b346dbaa6acd05774da67158/'
-                frameBorder='0'
-                width='100%'
-                height='100%'
-            ></iframe>
+            <Spline
+                scene="https://prod.spline.design/PBcOzeNKKp5lmiCW/scene.splinecode" 
+            />
             <div style={{ position: 'absolute', top: '10px', left: '10px' }}>
                 <Icon icon="mdi:keyboard-w" /> W
                 <Icon icon="mdi:keyboard-s" /> S
                 <Icon icon="mdi:keyboard-a" /> A
                 <Icon icon="mdi:keyboard-d" /> D
                 <Icon icon="mdi:space-invaders" /> Space
+                
             </div>
             <div style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
                 Speed: 0
