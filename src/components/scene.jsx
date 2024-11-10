0f20b1821034549c884ad51dd1872b460001c640
ALPHISTUBE <rsalphi@gmail.com>
2024-11-10 11:55:07 +0600

    update

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -5,7 +5,6 @@ import letter_d from '../../public/letter-d.png';
 import letter_s from '../../public/letter-s.png';
 import letter_w from '../../public/letter-w.png';
 import space from '../../public/space.png';
-import SpeedComponent from './speed';
 import Image from 'next/image';
 import Spline from '@splinetool/react-spline/next';
 const Scene = () => {
@@ -14,15 +13,14 @@ const Scene = () => {
             <Spline
                 scene="https://prod.spline.design/PBcOzeNKKp5lmiCW/scene.splinecode" 
             />
-            <div className="absolute top-1/2 left-[5rem] transform -translate-x-1/2 -translate-y-1/2 p-2 rounded text-white flex flex-col gap-1 ml-5">
-                <div className=' bg-black bg-opacity-50 px-3 py-2 text-right'>
-                    <div className="flex items-center w-[100%] px-5"><Image className='w-8 h-8 filter invert' src={letter_w} alt="W" /><p className="ml-2 text-base font-bold">W</p></div>
-                    <div className="flex items-center w-[100%] px-5"><Image className='w-8 h-8 filter invert' src={letter_s} alt="S" /><p className="ml-2 text-base font-bold">S</p></div>
-                    <div className="flex items-center w-[100%] px-5"><Image className='w-8 h-8 filter invert' src={letter_a} alt="A" /><p className="ml-2 text-base font-bold">A</p></div>
-                    <div className="flex items-center w-[100%] px-5"><Image className='w-8 h-8 filter invert' src={letter_d} alt="D" /><p className="ml-2 text-base font-bold">D</p></div>
-                    <div className="flex items-center w-[100%] px-5"><Image className='w-12 h-10 filter invert' src={space} alt="Space" /><p className="ml-2 text-base font-bold">SPACE</p></div>
+            <div className="absolute top-1/2 left-[5rem] transform -translate-x-1/2 -translate-y-1/2 p-2 rounded text-white flex flex-col gap-1 ml-10">
+                <div className=' bg-black bg-opacity-50 px-1 py-2 shadow-sm shadow-stone-950'>
+                    <div className="flex justify-end items-center w-[100%] px-5"><p className="text-sm font-bold">FORWARD</p><Image className='w-8 h-8 filter invert ml-3' src={letter_w} alt="W" /></div>
+                    <div className="flex justify-end items-center w-[100%] px-5"><p className="text-sm font-bold">BACKWARD</p><Image className='w-8 h-8 filter invert ml-3' src={letter_s} alt="S" /></div>
+                    <div className="flex justify-end items-center w-[100%] px-5"><p className="text-sm font-bold">LEFT</p><Image className='w-8 h-8 filter invert ml-3' src={letter_a} alt="A" /></div>
+                    <div className="flex justify-end items-center w-[100%] px-5"><p className="text-sm font-bold">RIGHT</p><Image className='w-8 h-8 filter invert ml-3' src={letter_d} alt="D" /></div>
+                    <div className="flex justify-end items-center w-[100%] px-5"><p className="text-sm font-bold">JUMP</p><Image className='w-12 h-10 filter invert ml-3' src={space} alt="Space" /></div>
                 </div>
-                <SpeedComponent />
             </div>
         </div>
     );
